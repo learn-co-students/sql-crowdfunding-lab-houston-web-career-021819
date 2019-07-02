58f710075e807e3ff867387e78794b757acef709
github username <github email address>
2019-07-02 13:49:54 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+create TABLE users(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+create TABLE projects(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    funding_goal INTEGER,
+    start_date DATE,
+    end_date DATE
+);
+
+create TABLE pledges(
+    id INTEGER PRIMARY KEY,
+    amount FLOAT,
+    user_id INTEGER,
+    project_id INTEGER 
+);
